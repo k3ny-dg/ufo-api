1ff68334302b49e52ae8f91aba4b09e07d1613ea
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-01-13 20:36:42 -0800

    Add main directory.

---
+++ src/main/java/edu/greenriver/sdev/saasproject/SaasProjectApplication.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.saasproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SaasProjectApplication.class, args);
+    }
+
+}
